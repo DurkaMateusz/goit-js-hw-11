1e240b495899e51f8541fd246d9ba40c94a23821
DurkaMateusz <mosca888@gmail.com>
2023-12-16 18:10:59 +0100

    feat: added library of notiflix, simplelightbox xand simplelightbox.css, all basic ready to coding

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const searchForm = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
 const loadMoreBtn = document.getElementById('load-more');
