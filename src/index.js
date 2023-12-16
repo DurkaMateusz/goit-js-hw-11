50da8a065a5d5e8f5470694021f7bc854153fdba
DurkaMateusz <mosca888@gmail.com>
2023-12-16 18:14:44 +0100

    feat: added api key and api url

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const searchForm = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
 const loadMoreBtn = document.getElementById('load-more');
+
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '41298074-cfd64eaa637c088c07f5acf73';
