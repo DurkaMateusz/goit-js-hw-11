08ea593fb024ee5c372600ee3806054136f0bde4
DurkaMateusz <mosca888@gmail.com>
2023-12-16 17:59:49 +0100

    feat: changes in html button and div gallery, changes in index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const searchForm = document.getElementById('search-form');
+const gallery = document.getElementById('gallery');
+const loadMoreBtn = document.getElementById('load-more');
