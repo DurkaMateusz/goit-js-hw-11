33f11910feeaab5cf57ac4531d7bff0c1c7e8a36
DurkaMateusz <mosca888@gmail.com>
2023-12-17 00:24:54 +0100

    feat:added few limitations from hw musts, did working button load more, put some cosmetic changes, im think its finish

--- src/index.js
+++ src/index.js
@@ -10,15 +10,22 @@ const loadMoreBtn = document.getElementById('load-more');
 let searchQuery = '';
 let currentPage = 1;
 let totalHits = 0;
+let counterSubmit = 0;
 
 const API_KEY = '41266476-bb46a0bfc74cc3a1da8946be1';
 const API_URL = 'https://pixabay.com/api/';
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadMoreBtn.style.display = 'none';
+});
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   searchQuery = event.target.searchQuery.value;
   currentPage = 1;
   gallery.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
+  counterSubmit = 0;
   fetchImages();
 });
 
@@ -26,10 +33,46 @@ async function fetchImages() {
   const url = `${API_URL}?key=${API_KEY}&q=${encodeURIComponent(
     searchQuery
   )}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
+  try {
     const response = await axios.get(url);
     const data = response.data;
-    displayImages(data.hits); 
-};
+    totalHits = data.totalHits;
+    if (data.hits.length === 0 && currentPage === 1) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMoreBtn.style.display = 'none';
+      return false;
+    } else if (data.hits.length === 0) {
+      return false;
+    } else {
+      displayImages(data.hits);
+      loadMoreBtn.style.display =
+        gallery.children.length < totalHits ? 'block' : 'none';
+        if(counterSubmit === 0) {
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);   
+        }
+        counterSubmit ++;  
+      return true;
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    Notiflix.Notify.failure('Error fetching images. Please try again later.');
+    return false;
+  }
+}
+
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage++;
+  const hasMoreImages = await fetchImages();
+
+  if (!hasMoreImages && gallery.children.length >= totalHits) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    loadMoreBtn.style.display = 'none';
+  }
+});
 
 function displayImages(images) {
   images.forEach(image => {
@@ -48,5 +91,13 @@ function displayImages(images) {
     `;
     gallery.appendChild(photoCard);
   });
-  new SimpleLightbox('.gallery a', {}).refresh();
-}
+    new SimpleLightbox('.gallery a', {}).refresh();
+    if (gallery.children.length >= totalHits) {
+    loadMoreBtn.style.display = 'none';
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+};
+
+
